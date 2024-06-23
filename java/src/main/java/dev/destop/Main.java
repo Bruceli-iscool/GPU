11b69d97095a20f19e36a539715166256f371abd
Bruce Li <isanoob1897@outlook.com>
2024-06-23 10:22:43 -0700

    add

--- java/src/main/java/dev/destop/Main.java
+++ java/src/main/java/dev/destop/Main.java
@@ -1,7 +1,21 @@
 package dev.destop;
 
+import com.aparapi.Kernel;
+import com.aparapi.Range;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        final float inA[] = {1, 2, 4, 5 ,6 , 7};
+        final float inB[] = {5, 6, 7, 3, 5};
+        Kernel kernel = new Kernel() {
+            @Override
+            public void run() {
+                int i = getGlobalId();
+                result[i] = inA[i] + inB[i];
+            }
+        };
+
+        Range range = Range.create(result.length);
+        kernel.execute(range);
     }
 }
